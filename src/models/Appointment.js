5617a388623f7746bb3b601751c36ab984a6c4a1
= <anikethpala@gmail.com>
2025-01-11 19:36:36 +0530

    Changed indexing method to index Id individually in appointment model

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose');
 
-/* studentId and professorId are indexed together and required, index is used to speed up the search as mostly 
-we are searching by studentId and professorId */
-
+//studentId and professorId are indexed separately, as we are filtering slots by professorId and studentId individually
 const appointmentSchema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    professorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true, required: true },
+    professorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true, required: true },
     date: { type: Date, required: true },
     timeSlot: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
-
-// Compound index for studentId and professorId
-appointmentSchema.index({ studentId: 1, professorId: 1 });
   
 module.exports = mongoose.model('Appointments', appointmentSchema);
